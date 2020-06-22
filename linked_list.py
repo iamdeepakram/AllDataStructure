def5ee587d90b87197942f2c24c183ec4b01a53d
iamdeepakram <deepaksutrakar@aol.com>
2020-06-22 23:40:07 +0530

    added linked list simple code

---
+++ linked_list.py
@@ -0,0 +1,35 @@
+# We Create linked list 
+
+#Create class node 
+class Node:
+    
+    # Initiallize method
+    def __init__(self, data=None, next= None):
+        self.data = data
+        self.next = next
+
+    # basic mechanism for displaying the string type
+    def __str__(self):
+        return str(self.data)
+    
+#--- Assigning Nodes here ---# 
+NodeA = Node(2)
+NodeB = Node(3)
+NodeC = Node(4)
+NodeD = Node(5)
+
+#--- Linking Nodes ---#
+
+# First node refer to second node
+NodeA.next = NodeB
+# Seond node refer to third node
+NodeB.next = NodeC
+# Third node refer to fourth node
+NodeC.next = NodeD
+
+# Print Nodes
+print(NodeA)
+
+# Traverse linked list
+# Insert element in linked list
+# Remove element in linked list
